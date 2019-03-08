6b19de4c427f004eec84951b9e11fbac95fd6b41
Ck T <chengkou@gmail.com>
2019-03-08 16:46:42 -0600

    add router

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+
+// Route includes
+const portfolioRouter = require('./routes/portfolio.router');
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 /** ---------- ROUTES ---------- **/
-
+app.use('/portfolio', portfolioRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
